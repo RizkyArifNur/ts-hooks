3cfcb2cffad0412407e2a909864f148b9c0cd929
RizkyArifNur <rizkyarifnur@gmail.com>
2019-04-23 11:10:05 +0700

    add some documentation

--- src/function-hooks/function-sync-hooks.ts
+++ src/function-hooks/function-sync-hooks.ts
@@ -1,5 +1,15 @@
 import { Events } from './event'
 
+/**
+ * add hooks to synchronous function
+ * @remarks
+ * this function will return a method decorators that will add hooks to the target function
+ * there is two events : `AFTER` and `BEFORE`
+ * events `AFTER` will execute the hooks after the target function is called
+ * and events `BEFORE` will execute the hooks before the target function is called
+ * @param events event when the hooks will called (`AFTER` | `BEFORE`)
+ * @param hooks list of hooks
+ */
 export function addHooks(
   events: Events,
   ...hooks: Array<(...args: any[]) => any>
@@ -9,19 +19,37 @@ export function addHooks(
     _key,
     descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
   ) => {
+    /**
+     * store the previous method, beacause we will call it later
+     */
     const originalFunction = descriptor.value
+    /**
+     * wrap the original function, and execute the hooks
+     */
     descriptor.value = function(...args: any[]) {
+      /**
+       * if the selected event is `AFTER` then we will execute the hooks before we call the original function
+       */
       if (events === 'BEFORE') {
         for (const hook of hooks) {
           hook.call(this, ...args)
         }
       }
+      /**
+       * call the original function with the arguments, and store the return value, because we'll return that later
+       */
       const result = originalFunction.call(this, ...args)
+      /**
+       * if the selected event is `BEFORE` then we will execute the hooks after we call the original function
+       */
       if (events === 'AFTER') {
         for (const hook of hooks) {
           hook.call(this, ...args)
         }
       }
+      /**
+       * return the result of invoked original method
+       */
       return result
     }
     return descriptor
