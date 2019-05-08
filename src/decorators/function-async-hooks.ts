1e2a92f7d9ef384844ef069582ca699d333e35b8
RizkyArifNur <rizkyarifnur@gmail.com>
2019-05-08 14:27:07 +0700

    fix catching error

--- src/decorators/function-async-hooks.ts
+++ src/decorators/function-async-hooks.ts
@@ -68,87 +68,83 @@ function addAsyncHooksAsMiddlewareDecorators(
      */
     const originalFunction = descriptor.value
     descriptor.value = async (...args: any[]) => {
-      try {
-        /**
-         * array that will be used for storing the proccess or function that
-         * will be executed in sequence
-         */
-        let processes = []
+      /**
+       * array that will be used for storing the proccess or function that
+       * will be executed in sequence
+       */
+      let processes = []
 
-        /**
-         * value that will be used for storing value that returned from taget function/original function
-         */
-        let returnedValue = null
+      /**
+       * value that will be used for storing value that returned from taget function/original function
+       */
+      let returnedValue = null
 
-        /**
-         * value that indicates where is the current process position in array `processes` above
-         */
-        let processPosition = 0
+      /**
+       * value that indicates where is the current process position in array `processes` above
+       */
+      let processPosition = 0
 
+      /**
+       * if this function invoked in hooks functions it will be
+       * executed the next process, otherwise the next functions/process will not executed,
+       * it's means that the process will be terminated/stopped
+       * @param nextArgs params that will be passed for the next functions/process
+       */
+      const next = (...nextArgs) => {
         /**
-         * if this function invoked in hooks functions it will be
-         * executed the next process, otherwise the next functions/process will not executed,
-         * it's means that the process will be terminated/stopped
-         * @param nextArgs params that will be passed for the next functions/process
+         * `next` callback can't be called in the last process
          */
-        const next = (...nextArgs) => {
+        if (processPosition !== processes.length - 1) {
           /**
-           * `next` callback can't be called in the last process
+           * increment the process position,
+           * because we will executed the next process
            */
-          if (processPosition !== processes.length - 1) {
-            /**
-             * increment the process position,
-             * because we will executed the next process
-             */
-            processPosition++
-            const nextProcess = processes[processPosition]
-            /**
-             * if `next` callback called without an argument so we will fill the argument with
-             * the previous arguments
-             */
-            const nextArguments = nextArgs.length > 0 ? nextArgs : args
-            if (nextProcess === originalFunction) {
-              nextProcess.call(this, ...nextArguments).then(result => {
-                returnedValue = result
-                /**
-                 * if the next process is the target function / original function
-                 * we will call `next` callback manually, because we not provide `next` callback to the
-                 * original function
-                 */
-                next(...nextArguments)
-              })
-            } else {
-              nextProcess.call(this, next, ...nextArguments)
-            }
+          processPosition++
+          const nextProcess = processes[processPosition]
+          /**
+           * if `next` callback called without an argument so we will fill the argument with
+           * the previous arguments
+           */
+          const nextArguments = nextArgs.length > 0 ? nextArgs : args
+          if (nextProcess === originalFunction) {
+            nextProcess.call(this, ...nextArguments).then(result => {
+              returnedValue = result
+              /**
+               * if the next process is the target function / original function
+               * we will call `next` callback manually, because we not provide `next` callback to the
+               * original function
+               */
+              next(...nextArguments)
+            })
+          } else {
+            nextProcess.call(this, next, ...nextArguments)
           }
         }
-
-        /**
-         * fill the process with combinations of `hooksBefore`,`original function`,`hooks after`
-         */
-        processes = processes.concat(hooksBefore)
-        processes.push(originalFunction)
-        processes = processes.concat(hooksAfter)
-        /**
-         * get the current process that will be executed
-         */
-        const process = processes[processPosition]
-        /**
-         * if the current process is orginal function then
-         * we will not provides the `next` callback
-         * and invoked `next` callback manually
-         */
-        if (process === originalFunction) {
-          returnedValue = await process.call(this, ...args)
-          next(...args)
-        } else {
-          await process.call(this, next, ...args)
-        }
-
-        return returnedValue
-      } catch (error) {
-        console.log(error)
       }
+
+      /**
+       * fill the process with combinations of `hooksBefore`,`original function`,`hooks after`
+       */
+      processes = processes.concat(hooksBefore)
+      processes.push(originalFunction)
+      processes = processes.concat(hooksAfter)
+      /**
+       * get the current process that will be executed
+       */
+      const process = processes[processPosition]
+      /**
+       * if the current process is orginal function then
+       * we will not provides the `next` callback
+       * and invoked `next` callback manually
+       */
+      if (process === originalFunction) {
+        returnedValue = await process.call(this, ...args)
+        next(...args)
+      } else {
+        await process.call(this, next, ...args)
+      }
+
+      return returnedValue
     }
   }
 }
